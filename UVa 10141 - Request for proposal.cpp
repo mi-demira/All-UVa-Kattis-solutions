9f6088f75fe9cc372458e8d5932316d911fb2520
mi-demira <pijolina1@gmail.com>
2026-07-31 21:18:50 +0300

    Add files via upload

---
+++ UVa 10141 - Request for proposal.cpp
@@ -0,0 +1,77 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	int n; //broi iziskvaniq
+	int p; //broi predlojeniq
+
+	int test = 0;
+
+	while (cin >> n >> p && n && p != 0)
+	{
+		cin.ignore();
+		test++;
+		vector <string> iziskvaniq(n);
+	
+
+		//chetene na iziskvaniqta
+		for (int i = 0; i < n;i++)
+		{
+			getline(cin, iziskvaniq[i]);
+		}
+
+		//za da zapazim nai-dobriqt variant
+		string bestName;
+		double bestCompliance = -1.0; //procent
+		double bestPrice = 0;
+	
+
+		//vseki proporsal
+		for (int j = 0; j < p; j++)
+		{
+			string name;
+			float d; //cena
+			int r; //iziskvaniq koito se pokrivat
+
+			getline(cin, name);
+			cin >> d >> r;
+			cin.ignore();
+		
+
+			//chetene na pokriti iziskvaniq
+			vector <string> pokritiIziskvaniq(r);
+			for (int k = 0; k < r;k++)
+			{
+				getline(cin, pokritiIziskvaniq[k]);
+			}
+
+			float numCompliances =(float) r / n;
+
+			//smenqme nai-dobroto predlojenie
+			if (numCompliances > bestCompliance)
+			{
+				bestCompliance = numCompliances;
+				bestPrice = d;
+				bestName = name;
+			}
+			//ako ednakvi req met , po cena
+			else if (numCompliances == bestCompliance && d < bestPrice)
+			{
+				bestPrice = d;
+				bestName = name;
+			}
+
+		}
+
+		if (test > 1)
+			cout << endl;
+
+		cout << "RFP #" << test << endl;
+		cout << bestName << endl;
+
+	}
+
+}
