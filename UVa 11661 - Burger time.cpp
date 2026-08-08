b7f5e541f465802e8de8614508365e723531ec25
mi-demira <pijolina1@gmail.com>
2026-08-08 18:32:25 +0300

    Add files via upload

---
+++ UVa 11661 - Burger time.cpp
@@ -0,0 +1,56 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    int lenghtHighway;
+    
+
+    while (true)
+    {
+        cin >> lenghtHighway;
+
+        if (lenghtHighway == 0)
+            break;
+
+        string positions;
+        cin >> positions;
+
+        int minD = lenghtHighway;
+        //vse oshte ne sa sreshtani
+        int lastD = -lenghtHighway;
+        int lastR = -lenghtHighway;
+
+        for (int j = 0; j < lenghtHighway;j++)
+        {
+            //ima i drugstore i resturant
+            if (positions[j] == 'Z')
+            {
+                minD = 0;
+            }
+            //ako stupim na drugstore
+            else if (positions[j] == 'D')
+            {
+                //razstoqnie ot tekushtoto D do posledno namerenoto R
+                minD = min(minD,j- lastR );
+                //zapomnqme poziciq posledno D
+                lastD = j;
+            }
+            else if (positions[j] == 'R')
+            {
+                //proverka za kude se namira poslednoto D
+                minD = min(minD,j- lastD);
+                //zapis poziciq posledno R
+                lastR = j;
+            }
+          
+        }
+        cout << minD << endl;
+
+     
+
+    }
+}
+
