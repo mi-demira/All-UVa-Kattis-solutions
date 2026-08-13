2f8e5274fe578b17e5b1f38096181c3131407aa4
mi-demira <pijolina1@gmail.com>
2026-08-13 17:01:27 +0300

    Add files via upload

---
+++ UVa 11687 - Digits.cpp
@@ -0,0 +1,45 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    string x0;
+    while (true)
+    {
+        cin >> x0;
+        if (x0 == "END")
+            break;
+
+        //chisloto suvpada s broq cifrite
+        if (x0=="1")
+        {
+            cout << 1 << endl;
+        }
+
+        int x1 = x0.length();
+
+        //purva stupka e veche napravena
+        int counter = 1;
+
+        while (true)
+        {
+            //duljina na segashno chislo
+            int nextX = to_string(x1).length();
+            counter++;
+
+            //ako sa ravni, prekusvame
+            if (nextX == x1)
+            {
+                break;
+            }
+
+            //novoto chislo stava x1
+            x1 = nextX;
+        }
+
+        cout << counter << endl;
+
+    }
+}
+
