1235db1867d3c95f1cde91bb5256566eb048cbfa
mi-demira <pijolina1@gmail.com>
2026-08-21 18:31:16 +0300

    Add files via upload

---
+++ UVa 13007 - D as in Daedalus.cpp
@@ -0,0 +1,45 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int N, M;
+
+	while (cin >> N >> M)
+	{
+		int B;
+		//num rounds
+		for (int i = 0;i < M;i++)
+		{
+			int sumP = 0;
+			//every round has diff budget
+			cin >> B;
+
+			//daedalus is always the value at index 0
+			vector <int> points;
+			for (int j = 0; j < N;j++)
+			{
+				int point;
+				cin >> point;
+				points.push_back(point);
+				sumP += point;
+
+			}
+
+			//kogato e zaguba, nqma nujda ot proverka
+			bool win = false;
+			if (sumP <= B)
+				win = true;
+
+			if (win)
+			{
+				int extraPoints= B - sumP;
+				cout << extraPoints << endl;
+			}
+
+			
+
+		}
+	}
+}
