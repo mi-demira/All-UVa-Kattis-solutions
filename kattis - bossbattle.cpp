52d7a5cdc04606fe9939ec076f6a35954e55e316
mi-demira <pijolina1@gmail.com>
2026-08-21 23:42:44 +0300

    Add files via upload

---
+++ kattis - bossbattle.cpp
@@ -0,0 +1,21 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	int pillars;
+	while (cin >> pillars)
+	{
+		//base case
+		if (pillars <= 3)
+		{
+			cout << "1" << endl;
+		}
+		//v purvi hod se unishtojava ne samo ocelenata, a i 2 koloni oshte
+		else if (pillars > 3)
+		{
+			cout << pillars - 2 << endl;
+		}
+	}
+ 
+}
