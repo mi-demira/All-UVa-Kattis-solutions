82190729850dc277a0b03154783ccb62c62b658e
mi-demira <pijolina1@gmail.com>
2026-08-21 19:35:45 +0300

    Add files via upload

---
+++ kattis - army strenght (easy).cpp
@@ -0,0 +1,49 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int ng, nm;
+		cin >> nm >> ng;
+
+		int godzilaP = 0;
+		int mechaGP = 0;
+
+
+		//sum godzila and mecha power 
+
+		for (int i = 0; i < ng; i++)
+		{
+			int power;
+			cin >> power;
+			godzilaP += power;
+		} 
+
+		for (int i = 0; i < nm; i++)
+		{
+			int power;
+			cin >> power;
+			mechaGP += power;
+		}
+
+
+		if (ng > nm)
+		{
+			cout << "Godzilla" << endl;
+		}
+		else if (nm > ng)
+		{
+			cout << "MechaGodzilla" << endl;
+		}
+		else
+		{
+			cout << "uncertain" << endl;
+		}
+
+	}
+}
