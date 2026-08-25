38276fcd3a1bdb9f6f37eebcceb6b32023af7ce9
mi-demira <pijolina1@gmail.com>
2026-08-25 22:36:21 +0300

    Add files via upload

---
+++ kattis - exactly electrical.cpp
@@ -0,0 +1,27 @@
+﻿using namespace std;
+#include <iostream>
+
+
+int main()
+{
+    //starting and end coordinates
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+
+    int t;//battery
+    cin >> t;
+
+    //manhattan distance - nagore nadolu, nalqvo nadqsno
+    int distance = abs(c - a) + abs(d - b);
+
+    //trqbva da se izrazhodi v u turns
+    int izlishniD = t - distance;
+    
+    //ako izlishnite ne sa chetni, nqma kak da izrazhodim bateriqta
+    if (t >= distance && izlishniD % 2 == 0)
+        cout << "Y";
+    else
+        cout << "N";
+}
+
