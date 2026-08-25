f9a4faf636967abf7c450019ba4b942cd9ee4055
mi-demira <pijolina1@gmail.com>
2026-08-25 21:03:33 +0300

    Add files via upload

---
+++ kattis - event planning.cpp
@@ -0,0 +1,41 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    int N, B, H, W;
+    int fullPrice = 0;
+    
+    cin >> N >> B >> H >> W;
+
+    int minPrice = INT_MAX;
+    for (int i = 0; i < H;i++)
+    {
+        int priceP;
+        cin >> priceP;
+
+        for (int j = 0; j < W; j++)
+        {
+            int beds;
+            cin >> beds;
+
+          
+            if(beds >=N)
+            {
+                fullPrice = beds * priceP;
+
+                if (fullPrice < minPrice)
+                    minPrice = fullPrice;
+            }
+
+
+        }
+    }
+    if (minPrice > B)
+    {
+        cout << "stay home" << endl;
+    }
+    else
+    cout << minPrice << endl;
+}
+
