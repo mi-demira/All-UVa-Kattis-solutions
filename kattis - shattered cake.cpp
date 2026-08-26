583fb859ac98fe3216d80bd41e4d586bab903585
mi-demira <pijolina1@gmail.com>
2026-08-26 15:41:15 +0300

    Add files via upload

---
+++ kattis - shattered cake.cpp
@@ -0,0 +1,29 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+	int widthWhole;
+	long long N;
+
+	cin >> widthWhole;
+	cin >> N;
+
+	long long ploshtObshto=0;
+
+	for (int i = 0; i < N;i++)
+	{
+		long w, h;
+		cin >> w >> h;
+
+		//dobavqme ploshta na segashno parche
+		ploshtObshto += w * h;
+	}
+
+	//duljinata na cqlata torta
+	long long heightWhole = ploshtObshto / widthWhole;
+	cout << heightWhole << endl;
+
+}
